0300bb9636473a3fa5ab5cd20f9807a8e057d91f
shreyas12official <shreyas12senthil@gmail.com>
2025-06-04 15:06:18 +0530

    Time: 0 ms (100.00%) | Memory: 18 MB (3.91%) - LeetSync

--- 1329-minimum-cost-to-move-chips-to-the-same-position/minimum-cost-to-move-chips-to-the-same-position.py
+++ 1329-minimum-cost-to-move-chips-to-the-same-position/minimum-cost-to-move-chips-to-the-same-position.py
@@ -1,10 +1,10 @@
 class Solution:
     def minCostToMoveChips(self, position: List[int]) -> int:
-        even_count = 0
-        odd_count = 0
-        for pos in position:
-            if pos % 2 == 0:
-                even_count += 1
+        ec = 0
+        oc = 0
+        for i in position:
+            if i % 2 == 0:
+                ec += 1
             else:
-                odd_count += 1
-        return min(even_count, odd_count)
+                oc+= 1
+        return min(ec, oc)
